5a3002da25a1da1737ecda073c84a05e5b046cc5
Oguz Bilgener <oguzbilgener@gmail.com>
2015-12-03 11:05:35 +0200

    Delegate windows creation to GameController

--- src/App.java
+++ src/App.java
@@ -1,21 +1,15 @@
+import controller.GameController;
 import ui.GameWindow;
 
 public class App {
+
 	public static void main(String[] args) {
 
-		javax.swing.SwingUtilities.invokeLater(() -> createAndShowGUI());
+		GameController controller = new GameController();
+
+		javax.swing.SwingUtilities.invokeLater(() -> controller.createAndShowGUI());
 
 	}
 
-	private static void createAndShowGUI() {
-		//Create and set up the window.
-		GameWindow frame = new GameWindow();
-		frame.setDefaultCloseOperation(GameWindow.EXIT_ON_CLOSE);
-		frame.setTitle("Draw It!");
 
-		frame.getContentPane();
-		//Display the window.
-		frame.pack();
-		frame.setVisible(true);
-	}
 }
--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -1,7 +1,23 @@
 package controller;
 
+import ui.GameWindow;
+
+import javax.swing.*;
+
 /**
  * Created by oguzb on 03/12/15.
  */
 public class GameController {
+
+	public static void createAndShowGUI() {
+		//Create and set up the window.
+		GameWindow frame = new GameWindow();
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setTitle("Draw It!");
+
+		frame.getContentPane();
+		//Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
 }
