1142938777c50a57e3e86677dc995522e5b117d2
Oguz Bilgener <oguzbilgener@gmail.com>
2015-12-05 15:34:51 +0200

    Begin implementing P2P

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,10 +1,48 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author oguzb
  */
 public class Player {
-	public Player() {
 
+	protected List<String> addresses;
+    protected String username;
+    protected String password;
+
+	public Player() {
+        this.addresses = new ArrayList<>();
 	}
+
+    public Player(List<String> addresses, String username, String password) {
+        this.addresses = addresses;
+        this.username = username;
+        this.password = password;
+    }
+
+    public List<String> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<String> addresses) {
+        this.addresses = addresses;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
---
+++ src/network/P2PManager.java
@@ -0,0 +1,12 @@
+package network;
+
+/**
+ * @author oguzb
+ */
+public class P2PManager {
+
+    public static final int LISTEN_PORT = 4599;
+
+    public P2PManager() {
+    }
+}
